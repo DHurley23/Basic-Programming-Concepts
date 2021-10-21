ca39646614f9bcf64ccf2fb9f2769d6cd142f545
Darragh <darragh.hurley23@gmail.com>
2021-10-21 11:29:39 +0100

    Assignment 1 - MThree training (Altered RockPaperScissors.java to use more methods)

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -12,38 +12,26 @@ public class RockPaperScissors {
     private static final String USER_NAME = "User";
     private static final String CPU_NAME = "CPU";
     private static final String TIE = "Tie";
-
+    private static final Random RAND_NUM = new Random();
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args){
         playRockPaperScissors();
     }
 
-
     private static void playRockPaperScissors(){
         boolean playing = true;
-        Scanner myScanner = new Scanner(System.in);
         while(playing) {
             int cpuWins = 0;
             int userWins = 0;
             int ties = 0;
             System.out.println("Please enter the number of rounds you would like to play (between 1 and 10): ");
-            int rounds = myScanner.nextInt();
+            int rounds = SCANNER.nextInt();
             if (rounds >= MINIMUM_ROUNDS && rounds <= MAXIMUM_ROUNDS) {
                 for (int roundsCompleted = 0; roundsCompleted < rounds; roundsCompleted++) {
-                    int userSelection = -1;
-                    System.out.println("Please enter '1' for Rock, '2' for Paper or '3' for Scissors: ");
-                    while(userSelection != ROCK && userSelection != PAPER && userSelection != SCISSORS) {
-                        userSelection = myScanner.nextInt();
-                        if (userSelection > 3 || userSelection < 1){
-                            System.out.println("Invalid input - Try again");
-                        }
-                    }
-                    printOutput(USER_NAME, userSelection);
-
-                    Random randNum = new Random();
-                    int cpuSelection = randNum.nextInt(RANDOM_MAX_VALUE) + 1;
+                    int userSelection = makeUserMove();
+                    int cpuSelection = RAND_NUM.nextInt(RANDOM_MAX_VALUE) + 1;
                     printOutput(CPU_NAME, cpuSelection);
-
                     String outcome = returnResult(userSelection, cpuSelection);
                     switch(outcome){
                         case TIE:
@@ -61,27 +49,45 @@ public class RockPaperScissors {
                 }
                 printNumberOfWins(cpuWins, userWins, ties);
                 printWinner(userWins, cpuWins);
-                System.out.println("\nWould you like to play again? (Y/N): ");
-                myScanner.nextLine(); //throw away the \n not consumed by nextInt()
-                String response = "";
-                while (!response.equals("N") && !response.equals("n")) {
-                    response = myScanner.nextLine();
-                    if (response.equals("N") || response.equals("n")) {
-                        System.out.println("Thanks for playing!");
-                        playing = false;
-                    } else if (!response.equals("Y") && !response.equals("y")) {
-                        System.out.println("\nIncorrect input - Would you like to play again? (Y/N): ");
-                    }
-                    else {
-                        break;
-                    }
-                }
-
+                playing = playAgain();
             } else {
                 System.out.println("Incorrect input - game has quit automatically");
                 playing = false;
             }
         }
+    }
+
+    private static boolean playAgain() {
+        System.out.println("\nWould you like to play again? (Y/N): ");
+        SCANNER.nextLine(); //throw away the \n not consumed by nextInt()
+        String response = "";
+        boolean result = true;
+        while (!response.equals("N") && !response.equals("n") && !response.equals("Y") && !response.equals("y")) {
+            response = SCANNER.nextLine();
+            if (response.equals("N") || response.equals("n")) {
+                System.out.println("Thanks for playing!");
+                result = false;
+            } else if (!response.equals("Y") && !response.equals("y")) {
+                System.out.println("Incorrect input - Would you like to play again? (Y/N): ");
+            }
+            else{
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    private static int makeUserMove(){
+        int userSelection = -1;
+        System.out.println("Please enter '1' for Rock, '2' for Paper or '3' for Scissors: ");
+        while(userSelection != ROCK && userSelection != PAPER && userSelection != SCISSORS) {
+            userSelection = SCANNER.nextInt();
+            if (userSelection > 3 || userSelection < 1){
+                System.out.println("Invalid input - Try again");
+            }
+        }
+        printOutput(USER_NAME, userSelection);
+        return  userSelection;
     }
     private static void printOutput(String user, int input){
         switch (input) {
